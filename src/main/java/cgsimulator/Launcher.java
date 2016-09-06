da0e0feb022e73bea147cc9aa9bbf8caada858ef
David Giangiacomo <external.z09dgian@decathlon.com>
2016-09-06 13:47:40 +0200

    Global test creation -> currently KO

--- src/main/java/cgsimulator/Launcher.java
+++ src/main/java/cgsimulator/Launcher.java
@@ -8,9 +8,10 @@ public class Launcher {
 
     protected static GameSimulator gs = new GameSimulator();
 
-    public static void main(String[] args) throws IOException {
-        initGameSimulator(new ThorEp1Engine(), "/opt/trucmuche.jar");
-
+    public static void main(String... args) throws IOException {
+        initGameSimulator(new ThorEp1Engine(), args[0]);
+        gs.run();
+        System.out.println(gs.isWinning());
     }
 
     protected static void initGameSimulator(GameSimulator.GameEngine engine, String playerJarPath) throws IOException{
---
+++ src/test/java/cgsimulator/integration/CGSimulatorTest.java
@@ -0,0 +1,35 @@
+package cgsimulator.integration;
+
+import cgsimulator.Launcher;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CGSimulatorTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+        System.setErr(null);
+    }
+    @Test
+    public void should_win_thor() throws IOException {
+        Launcher.main(Thread.currentThread().getContextClassLoader().getResource("testable.jar").getPath());
+        assertThat(outContent.toString()).isEqualTo("true\n");
+    }
+}
