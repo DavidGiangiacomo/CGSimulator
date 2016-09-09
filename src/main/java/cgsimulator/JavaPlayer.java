1aac7cf825f3bfb91cb7d6a55c5250d1972849e1
Sylvain LAVAZAIS <external.Z06slava@decathlon.com>
2016-09-09 14:13:43 +0200

    fix(GameLauncher): add some TODO to fix the gameLaucher

--- src/main/java/cgsimulator/JavaPlayer.java
+++ src/main/java/cgsimulator/JavaPlayer.java
@@ -4,36 +4,36 @@ import java.io.*;
 
 public class JavaPlayer implements GameSimulator.GamePlayer {
 
-
     private final Process playerProcess;
     private final BufferedReader reader;
     private final BufferedWriter writer;
 
-    public JavaPlayer(String playerFileName) throws IOException {
-        ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", playerFileName);
+    public JavaPlayer(final String playerFileName) throws IOException {
+        // TODO:Java executable need to be given at the execute time (in 1 arg or 2 args)
+        final ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", playerFileName);
         playerProcess = processBuilder.start();
 
-        InputStream stdout = playerProcess.getInputStream();
-        OutputStream stdin = playerProcess.getOutputStream();
+        final InputStream stdout = playerProcess.getInputStream();
+        final OutputStream stdin = playerProcess.getOutputStream();
 
         reader = new BufferedReader(new InputStreamReader(stdout));
         writer = new BufferedWriter(new OutputStreamWriter(stdin));
     }
 
     @Override
-    public void initialize(String initInput) throws IOException {
+    public void initialize(final String initInput) throws IOException {
         writeToProcess(initInput);
     }
 
     @Override
-    public void setInput(String input) throws IOException {
+    public void setInput(final String input) throws IOException {
         writeToProcess(input);
     }
 
-    private void writeToProcess(String input) throws IOException {
+    private void writeToProcess(final String input) throws IOException {
         writer.write(input);
         writer.write("\n");
-        writer.flush();
+        // writer.flush();
     }
 
     @Override
--- src/main/java/cgsimulator/Launcher.java
+++ src/main/java/cgsimulator/Launcher.java
@@ -1,20 +1,22 @@
 package cgsimulator;
 
-import cgsimulator.engine.ThorEp1Engine;
-
 import java.io.IOException;
+
+import cgsimulator.engine.ThorEp1Engine;
 
 public class Launcher {
 
     protected static GameSimulator gs = new GameSimulator();
 
-    public static void main(String... args) throws IOException {
+    public static void main(final String... args) throws IOException {
         initGameSimulator(new ThorEp1Engine(), args[0]);
         gs.run();
         System.out.println(gs.isWinning());
     }
 
-    protected static void initGameSimulator(GameSimulator.GameEngine engine, String playerJarPath) throws IOException{
+    protected static void initGameSimulator(final ThorEp1Engine engine, final String playerJarPath) throws IOException {
+        // TODO: engine need to be standardized for the launch sequence
+        // engine.initialize(5, 4, 31, 4, 100);
         gs.setGameEngine(engine);
         gs.setPlayer(new JavaPlayer(playerJarPath));
     }
